bdae42b7d6619bb2db8a9cf7b89cf1081939c2ab
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-11 22:19:22 +0300

    BaseFragment.kt was implemetationed succesfully to DetailFragment.kt

--- app/src/main/java/com/oguzdogdu/recipes/presentation/detailfragment/DetailFragment.kt
+++ app/src/main/java/com/oguzdogdu/recipes/presentation/detailfragment/DetailFragment.kt
@@ -3,40 +3,32 @@ package com.oguzdogdu.recipes.presentation.detailfragment
 import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.oguzdogdu.recipes.R
 import com.oguzdogdu.recipes.databinding.FragmentDetailBinding
+import com.oguzdogdu.recipes.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailFragment : Fragment(R.layout.fragment_detail) {
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
 
-    private lateinit var binding: FragmentDetailBinding
-    private val args: DetailFragmentArgs by navArgs<DetailFragmentArgs>()
+    // private val args: DetailFragmentArgs by navArgs<DetailFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentDetailBinding.bind(view)
         setData()
         backStack()
-
     }
-
 
     private fun setData() {
-         val recipeList = args.recipeArgs
-        binding.title.text = recipeList.title
+        // val recipeList = args.recipeArgs
+        // binding.title.text = recipeList.title
     }
-
     private fun backStack() {
         val callBack = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 findNavController().popBackStack()
             }
         }
-
         requireActivity().onBackPressedDispatcher.addCallback(callBack)
     }
 }
