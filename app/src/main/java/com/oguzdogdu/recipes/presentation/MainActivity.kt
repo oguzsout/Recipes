e10c5d1f7a98d19a18d57dfdddf27353c17142af
Oguzhan Dogdu <oguzd494@gmail.com>
2022-01-01 15:22:15 +0300

    Navigation action bar setup

--- app/src/main/java/com/oguzdogdu/recipes/presentation/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/recipes/presentation/MainActivity.kt
@@ -2,6 +2,10 @@ package com.oguzdogdu.recipes.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.oguzdogdu.recipes.R
 import com.oguzdogdu.recipes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,5 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navHost()
+    }
+    private fun navHost() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
