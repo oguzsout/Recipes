0f1e8dd12b6c1ca3ab47a397eb67ff0374e5e888
oguzsout <oguzd494@gmail.com>
2022-01-14 22:45:31 +0300

    ListViewModel.kt changes

--- app/src/main/java/com/oguzdogdu/recipes/presentation/listfragment/ListViewModel.kt
+++ app/src/main/java/com/oguzdogdu/recipes/presentation/listfragment/ListViewModel.kt
@@ -1,33 +1,40 @@
 package com.oguzdogdu.recipes.presentation.listfragment
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.oguzdogdu.recipes.domain.model.RecipeResponse
-import com.oguzdogdu.recipes.domain.repository.RecipeRepoInterface
+import com.oguzdogdu.recipes.domain.use_case.RecipeUseCase
 import com.oguzdogdu.recipes.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class ListViewModel @Inject constructor(private val repo: RecipeRepoInterface) : ViewModel() {
+class ListViewModel @Inject constructor(private val recipeUseCase: RecipeUseCase) : ViewModel() {
 
-    private val _response = MutableLiveData<Resource<RecipeResponse>>()
-    val recipeResponse: LiveData<Resource<RecipeResponse>>
-        get() = _response
+    private val _recipeList = MutableStateFlow<RecipeState>(RecipeState())
+    val recipeList: StateFlow<RecipeState> = _recipeList
 
     init {
-        getRecipes()
+        getRecipies()
     }
 
-    private fun getRecipes() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val result = repo.allRecipes()
-            _response.postValue(result)
-        }
+    private fun getRecipies() {
+        recipeUseCase().onEach {
+            when (it) {
+                is Resource.Loading -> {
+                    _recipeList.value = RecipeState(isLoading = true)
+                }
+                is Resource.Success -> {
+                    _recipeList.value = RecipeState(data = it.data)
+                }
+                is Resource.Error -> {
+                    _recipeList.value = RecipeState(error = it.message ?: "")
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 }
 
