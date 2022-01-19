046d071d9837de0c63ccc54c0535761b1d47d4fb
oguzsout <oguzd494@gmail.com>
2022-01-19 22:07:45 +0300

    fresco implemetation

--- app/src/main/java/com/oguzdogdu/recipes/RecipeApplication.kt
+++ app/src/main/java/com/oguzdogdu/recipes/RecipeApplication.kt
@@ -1,7 +1,13 @@
 package com.oguzdogdu.recipes
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RecipeApplication : Application()
+class RecipeApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
+    }
+}
