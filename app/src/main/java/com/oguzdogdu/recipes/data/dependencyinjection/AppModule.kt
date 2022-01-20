808fa5e3ee8c7c0297613f46825823398291e9ad
oguzsout <oguzd494@gmail.com>
2022-01-20 15:23:35 +0300

    okhttp implementation in AppModule.kt

--- app/src/main/java/com/oguzdogdu/recipes/data/dependencyinjection/AppModule.kt
+++ app/src/main/java/com/oguzdogdu/recipes/data/dependencyinjection/AppModule.kt
@@ -1,5 +1,6 @@
 package com.oguzdogdu.recipes.data.dependencyinjection
 
+import com.oguzdogdu.recipes.BuildConfig
 import com.oguzdogdu.recipes.data.remote.RecipeInterface
 import com.oguzdogdu.recipes.data.repository.RecipeRepoImpl
 import com.oguzdogdu.recipes.domain.repository.RecipeRepoInterface
@@ -8,6 +9,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,13 +20,63 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 
 object AppModules {
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(): Interceptor {
+        return Interceptor { chain: Interceptor.Chain ->
+            val initialRequest = chain.request()
+
+            val newUrl = initialRequest.url.newBuilder()
+                .addQueryParameter("api_key", BuildConfig.API_KEY)
+                .build()
+
+            val newRequest = initialRequest.newBuilder()
+                .url(newUrl)
+                .build()
+
+            chain.proceed(newRequest)
+        }
+    }
 
     @Singleton
     @Provides
-    fun provideRetrofit(): RecipeInterface {
-        return Retrofit.Builder().baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()).build()
-            .create(RecipeInterface::class.java)
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        // Retrofit completely relies on OkHttp for any network operation.
+        // Since logging isn’t integrated by default anymore in Retrofit 2,
+        // we'll use a logging interceptor for OkHttp.
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: Interceptor,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRecipeService(retrofit: Retrofit): RecipeInterface {
+        return retrofit.create(RecipeInterface::class.java)
     }
 
     @Singleton
